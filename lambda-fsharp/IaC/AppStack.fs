a471daede9055cd7995c9ffc254ebd00141e47e2
Rajiv Mounguengue <rajiv.mounguengue@functionalstack.io>
2020-11-17 15:09:54 +0100

    chore: wip

--- lambda-fsharp/IaC/AppStack.fs
+++ lambda-fsharp/IaC/AppStack.fs
@@ -7,20 +7,29 @@ type EnvStackProps =
     inherit IStackProps
     abstract Production: bool
 
-type AppStack(scope, id, props: EnvStackProps) as this =
+//type AppStack(scope, id, props: EnvStackProps) as this =
+type AppStack(scope, id, props: StackProps, isProd: bool) as this =
     inherit Stack(scope, id, props)
 
     // The code that defines your stack goes here
     // Defining the prod or no prod
 
     let stackParams =
-        match props with
-        | props when props.Production ->
+        match isProd with
+        | true ->
             {| DynamoDbReadWrite = 200.
                TableName = "Cdk-Customers-Production" |}
-        | props when props.Production |> not ->
+        | false ->
             {| DynamoDbReadWrite = 5.
                TableName = "Cdk-Customers-Staging" |}
+
+        //match props with
+        //| props when props.Production ->
+        //    {| DynamoDbReadWrite = 200.
+        //       TableName = "Cdk-Customers-Production" |}
+        //| props when props.Production |> not ->
+        //    {| DynamoDbReadWrite = 5.
+        //       TableName = "Cdk-Customers-Staging" |}
 
     // --- dynamodb ---
     let table =
@@ -28,10 +37,6 @@ type AppStack(scope, id, props: EnvStackProps) as this =
             TableProps
                 (TableName = stackParams.TableName,
                  ReadCapacity = stackParams.DynamoDbReadWrite,
-                 PartitionKey =
-                     { new IAttribute with
-                         member _.Type = AttributeType.STRING
-                         member _.Name = "PK" },
+                 PartitionKey = new Attribute(Type = AttributeType.STRING, Name = "PK"),
                  BillingMode = BillingMode.PROVISIONED)
-        //tableProps.PartitionKey <- {IA}
         Table(this, "customers", tableProps)
