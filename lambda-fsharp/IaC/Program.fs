0ba944cabda2cc46ef20fac9327374cafb1b0fae
Rajiv Mounguengue <rajiv.mounguengue@functionalstack.io>
2020-11-17 15:41:29 +0100

    chore: wip

--- lambda-fsharp/IaC/Program.fs
+++ lambda-fsharp/IaC/Program.fs
@@ -5,33 +5,30 @@ open App
 let main _ =
     let app = App(null)
 
-    AppStack(app, "cdk-customers-prod", StackProps(Env = {new IEnvironment with member _.Region = "eu-west-2"}), true) |> ignore
+    AppStack(app, "cdk-customers-prod", StackProps(Env = new Environment(Region = "eu-west-2")), true) |> ignore
 
-    AppStack
-        (app,
-         "cdk-customers-staging", StackProps(Env = {new IEnvironment with member _.Region = "eu-west-2"}), false)
-    |> ignore
+    AppStack(app, "cdk-customers-staging", StackProps(Env = new Environment(Region = "eu-west-2")), false) |> ignore
 
     //AppStack
-    //(app,
-    // "cdk-customers-prod",
-    // { new EnvStackProps with
-    //     member _.Production = true
+    //    (app,
+    //     "cdk-customers-prod",
+    //     { new EnvStackProps with
+    //         member _.Production = true
 
-    //     member _.Env =
-    //         { new IEnvironment with
-    //             member _.Region = "eu-west-2" } })
+    //         member _.Env =
+    //             { new IEnvironment with
+    //                 member _.Region = "eu-west-2" } })
     //|> ignore
 
     //AppStack
-    //(app,
-    // "cdk-customers-staging",
-    // { new EnvStackProps with
-    //     member _.Production = false
+    //    (app,
+    //     "cdk-customers-staging",
+    //     { new EnvStackProps with
+    //         member _.Production = false
 
-    //     member _.Env =
-    //         { new IEnvironment with
-    //             member _.Region = "eu-west-2" } })
+    //         member _.Env =
+    //             { new IEnvironment with
+    //                 member _.Region = "eu-west-2" } })
     //|> ignore
 
     app.Synth() |> ignore
