9dd321c67d7bd4e7558d6f2e79e358c0c13eacca
Rajiv Mounguengue <rajiv.mounguengue@functionalstack.io>
2020-11-18 17:00:36 +0100

    Update PipelineStack.fs

--- lambda-fsharp/IaC/PipelineStack.fs
+++ lambda-fsharp/IaC/PipelineStack.fs
@@ -2,6 +2,7 @@ namespace Pipeline
 
 open Amazon.CDK
 open Amazon.CDK.Pipelines
+open Amazon.CDK.AWS.CodeBuild
 open Amazon.CDK.AWS.CodePipeline
 open Amazon.CDK.AWS.CodePipeline.Actions
 
@@ -12,36 +13,42 @@ type EnvStackProps =
 type PipelineStack(scope, id, props: StackProps) as this =
     inherit Stack(scope, id, props)
 
-    // --- repository ---
     let sourceArtefact = Artifact_()
     let cloudAssemblyArtifact = Artifact_()
 
+    let sourceAction =
+        GitHubSourceAction
+            (GitHubSourceActionProps
+                (ActionName = "GitHub",
+                 Output = sourceArtefact,
+                 OauthToken = SecretValue.SecretsManager("github-token"),
+                 Branch = "master",
+                 Owner = "Rajivhost",
+                 Repo = "aws-cdk-iac",
+                 Trigger = GitHubTrigger.POLL))
+
+    let synthAction =
+        SimpleSynthAction
+            (SimpleSynthActionProps
+                (SourceArtifact = sourceArtefact,
+                 CloudAssemblyArtifact = cloudAssemblyArtifact,
+                 Environment = BuildEnvironment((*BuildImage = LinuxBuildImage.STANDARD_2_0,*) Privileged = true),
+                 InstallCommands =
+                     [| "npm install -g aws-cdk"
+                        "wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb"
+                        "dpkg -i packages-microsoft-prod.deb"
+                        "apt-get update"
+                        "apt-get install -y apt-transport-https && apt-get update && apt-get install -y dotnet-sdk-5.0" |],
+                 BuildCommands = [| "dotnet build" |],
+                 SynthCommand = "cdk synth"))
+
+
     let cdkPipelineProps =
         CdkPipelineProps
-            (CloudAssemblyArtifact = cloudAssemblyArtifact,
-             PipelineName = "Cdk-Customers-Pipeline",
-             SourceAction =
-                 GitHubSourceAction
-                     (GitHubSourceActionProps
-                         (ActionName = "GitHub",
-                          Output = sourceArtefact,
-                          OauthToken = SecretValue.SecretsManager("github-token"),
-                          Owner = "Rajivhost",
-                          Repo = "aws-cdk-iac",
-                          Trigger = GitHubTrigger.POLL)),
-             SynthAction =
-                 SimpleSynthAction
-                     (SimpleSynthActionProps
-                         (SourceArtifact = sourceArtefact,
-                          CloudAssemblyArtifact = cloudAssemblyArtifact,
-                          InstallCommands =
-                              [| "npm install -g aws-cdk"
-                                 "npm install -g dotnet-sdk-3.1"
-                                 "dotnet restore" |],
-                          BuildCommands = [|"dotnet build"|],
-                          SynthCommand = "cdk synth"))
-
-            )
+            (PipelineName = "Cdk-Customers-Pipeline",
+             CloudAssemblyArtifact = cloudAssemblyArtifact,
+             SourceAction = sourceAction,
+             SynthAction = synthAction)
 
     let pipeline =
         CdkPipeline(this, "Pipeline", cdkPipelineProps)
